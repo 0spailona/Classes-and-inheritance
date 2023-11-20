f1e579392b6b0616805d040468a03e699ea1d620
Daria <n-d-p@mail.ru>
2023-11-20 16:11:05 +0300

    To clean code

--- src/js/test/characters.test.js
+++ src/js/test/characters.test.js
@@ -1,7 +1,3 @@
-//import test from 'jest';
-//import expect from 'jest';
-//import 'jest'
-
 import Character from '../Character';
 import Daemon from '../Daemon';
 import Magician from '../Magician';
@@ -10,71 +6,63 @@ import Undead from '../Undead';
 import Zombie from '../Zombie';
 import Bowman from '../Bowman';
 
-///---
-
-// const player = new Daemon('name');
-// console.log(player.health);
-
-// throw new Error("Stopped");
-/// //-----
-
 test('getting error for undefined type of characters', () => {
-  class AnotherCharacter extends Character {
-    constructor(name) {
-      super(name, 'AnotherCharacter');
+    class AnotherCharacter extends Character {
+        constructor(name) {
+            super(name, 'AnotherCharacter');
+        }
     }
-  }
 
-  expect(() => new AnotherCharacter('Name')).toThrow('Нет такого типа персонажа');
+    expect(() => new AnotherCharacter('Name')).toThrow('Нет такого типа персонажа');
 });
 
 const shortNamesList = [
-  [Daemon, ''],
-  [Magician, '1'],
-  [Swordsman, '10'],
-  [Bowman, ''],
-  [Undead, '1'],
-  [Zombie, '10'],
+    [Daemon, ''],
+    [Magician, '1'],
+    [Swordsman, '10'],
+    [Bowman, ''],
+    [Undead, '1'],
+    [Zombie, '10'],
 ];
 const handlerForShortNameTest = test.each(shortNamesList);
 
 handlerForShortNameTest('testing getting error for too short name - %s', (TypeCharacter, name) => {
-  expect(() => new TypeCharacter(name)).toThrow('Имя должно быть длиной более 2 символов');
+    expect(() => new TypeCharacter(name)).toThrow('Имя должно быть длиной более 2 символов');
 });
 
 const longNamesList = [
-  [Daemon, '12345678910'],
-  [Magician, '12345678910'],
-  [Swordsman, '12345678910'],
-  [Bowman, '12345678910'],
-  [Undead, '12345678910'],
-  [Zombie, '12345678910'],
+    [Daemon, '12345678910'],
+    [Magician, '12345678910'],
+    [Swordsman, '12345678910'],
+    [Bowman, '12345678910'],
+    [Undead, '12345678910'],
+    [Zombie, '12345678910'],
 ];
 const handlerForLongNameTest = test.each(longNamesList);
 
 handlerForLongNameTest('testing getting error for too long name - %s', (TypeCharacter, name) => {
-  expect(() => new TypeCharacter(name)).toThrow('Имя должно быть длиной менее 10 символов');
+    expect(() => new TypeCharacter(name)).toThrow('Имя должно быть длиной менее 10 символов');
 });
 
 const charactersList = [
-  [Daemon],
-  [Magician],
-  [Swordsman],
-  [Bowman],
-  [Undead],
-  [Zombie],
+    [Daemon],
+    [Magician],
+    [Swordsman],
+    [Bowman],
+    [Undead],
+    [Zombie],
 ];
 
 const handlerForHealth = test.each(charactersList);
 
 handlerForHealth('testing getting health for %s', (TypeCharacter) => {
-  const player = new TypeCharacter('name');
-  expect(player.health).toBe(100);
+    const player = new TypeCharacter('name');
+    expect(player.health).toBe(100);
 });
 
 const handlerForLevel = test.each(charactersList);
 
 handlerForLevel('testing getting level for %s', (TypeCharacter) => {
-  const player = new TypeCharacter('name');
-  expect(player.level).toBe(1);
+    const player = new TypeCharacter('name');
+    expect(player.level).toBe(1);
 });
